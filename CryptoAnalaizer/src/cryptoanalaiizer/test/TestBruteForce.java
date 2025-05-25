a0de0a2c7aba459a25e7c876a3c23e6075150e71
Darfresor <vectnik@gmail.com>
2025-05-25 14:53:44 +0300

    добавлен словарь json

--- CryptoAnalaizer/src/cryptoanalaiizer/test/TestBruteForce.java
+++ CryptoAnalaizer/src/cryptoanalaiizer/test/TestBruteForce.java
@@ -1,17 +1,33 @@
 package cryptoanalaiizer.test;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import cryptoanalaiizer.model.Cipher;
 
+import java.io.InputStream;
 import java.util.HashSet;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashSet;
+import java.util.List;
 
 public class TestBruteForce {
     public static void main(String[] args) {
+        HashSet<String> DICTIONARY = null;
+        try {
+            // 1. Создаём ObjectMapper (основной класс Jackson)
+            ObjectMapper mapper = new ObjectMapper();
 
-        HashSet<String> DICTIONARY = new HashSet<>();
-        DICTIONARY.add("Это");
-        DICTIONARY.add("он");
-        DICTIONARY.add("тест");
+            // 2. Загружаем JSON-файл в Java-объект
+            InputStream inputStream = TestBruteForce.class
+                    .getResourceAsStream("/cryptoanalaiizer/resources/russian_top_1000.json");
 
+            List<String> words = mapper.readValue(inputStream, new TypeReference<List<String>>() {});
+            DICTIONARY = new HashSet<>(words);
+
+        } catch (Exception e) {
+            System.err.println("Ошибка при загрузке словаря:");
+            e.printStackTrace();
+        };
+        //-------------------------------------------
         Cipher cipher = new Cipher();
         String result;
         String bestResult = "";
@@ -23,7 +39,7 @@ public class TestBruteForce {
             result = cipher.decrypt("Яфр фжуф к рп струфрл!", i);
             String[] wordsinRelust = result.split("\\s+");
             for (String s : wordsinRelust) {
-                if (DICTIONARY.contains(s)) {
+                if (DICTIONARY.contains(s.toLowerCase())) {
                     System.out.printf("При работе с ключом = %d, слово \"%s\" найдено в словаре %n", i, s);
                     numberOfMatches++;
                 }
@@ -38,7 +54,10 @@ public class TestBruteForce {
         System.out.println("Наибольшое кол-во сопавдений = " + bestNumberOfMatches);
         System.out.println("При ключе  = " + finalkey);
         System.out.println(bestResult);
+        
+        
+    }
 
 
     }
-}
+
